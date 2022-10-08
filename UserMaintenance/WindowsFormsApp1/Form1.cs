4515bf111483943680f3a2e55417764e25aabdf5
AkosBencze01 <Akos.Bencze@stud.uni-corvinus.hu>
2022-10-08 14:27:42 +0200

    commit 2second commit

--- UserMaintenance/WindowsFormsApp1/Form1.cs
+++ UserMaintenance/WindowsFormsApp1/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
+
         public Form1()
         {
             InitializeComponent();
+            button1.Text = Resource1.Add;
+            label1.Text = Resource1.FirstName;
+            label1.Text = Resource1.LastName;
+
+            User temp = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(temp);
         }
     }
 }
