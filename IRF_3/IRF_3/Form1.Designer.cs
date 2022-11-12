d2d3e03a8414b8454166d73a8d024991d5048a1d
AkosBencze01 <Akos.Bencze@stud.uni-corvinus.hu>
2022-11-12 10:25:57 +0100

    first commit

---
+++ IRF_3/IRF_3/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace IRF_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(196, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(592, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(176, 381);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Ramens";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ IRF_3/IRF_3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_3
+{
+    public partial class Form1 : Form
+    {
+        List<Country> countries = new List<Country>();
+
+        List<Brand> brands = new List<Brand>();
+
+        List<Ramen> ramens = new List<Ramen>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData("ramen.csv");
+
+            GetCountries();
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "Name";
+
+            textBox1.TextChanged += TextBox1_TextChanged;
+
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var country = (Country)((ListBox)sender).SelectedItem;
+            if (country == null)
+                return;
+
+            var countryRamens = from r in ramens
+                                where r.CountryFK == country.ID
+                                select r;
+
+            var groupedRamens = from r in countryRamens
+                                group r.Rating by r.Brand.Name into g
+                                select new
+                                {
+                                    BrandName = g.Key,
+                                    AverageRating = Math.Round(g.Average(), 2)
+                                };
+
+            var orderedGroups = from g in groupedRamens
+                                orderby g.AverageRating descending
+                                select g;
+
+            dataGridView1.DataSource = orderedGroups.ToList();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetCountries();
+        }
+
+        void LoadData(string filename)
+        {
+            using (StreamReader sr = new StreamReader(filename, Encoding.Default))
+            {
+                sr.ReadLine(); // Header
+
+                while (!sr.EndOfStream)
+                {
+                    string[] adatok = sr.ReadLine().Split(';');
+
+                    var countryName = adatok[2];
+                    var brandName = adatok[0];
+                    Country result=AddCountry(countryName);
+                    Brand result2 = AddBrand(brandName);
+
+                    var newRamen = new Ramen()
+                    {
+                        ID = ramens.Count + 1,
+                        Brand = result2,
+                        Name = adatok[1],
+                        CountryFK = result.ID,
+                        Country = result,
+                        Rating = Convert.ToDouble(adatok[3])
+                    };
+                    ramens.Add(newRamen);
+                }
+            }
+        }
+
+        private Country AddCountry(string countryName)
+        {
+            var currentCountry = (from c in countries
+                                  where c.Name.Equals(countryName)
+                                  select c).FirstOrDefault();
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID = countries.Count + 1,
+                    Name = countryName
+                };
+                countries.Add(currentCountry);
+            }
+
+            return currentCountry;
+        }
+
+        private Brand AddBrand(string brandName)
+        {
+            var currentBrand = (from c in brands
+                                  where c.Name.Equals(brandName)
+                                  select c).FirstOrDefault();
+            if (currentBrand == null)
+            {
+                currentBrand = new Brand()
+                {
+                    ID = brands.Count + 1,
+                    Name = brandName
+                };
+                brands.Add(currentBrand);
+            }
+
+            return currentBrand;
+        }
+
+        private void GetCountries()
+        {
+            var countriesList = from c in countries
+                                where c.Name.ToLower().Contains(textBox1.Text.ToLower())
+                                orderby c.Name
+                                select c;
+            listBox1.DataSource = countriesList.ToList();
+        }
+    }
+}
